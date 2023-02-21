d42fd93ec22c294071616260e3e585d38d0c8d69
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-02-21 09:14:00 +0100

    Fix: the content was not displayed anymore correction of the display bug

--- single-formations.php
+++ single-formations.php
@@ -59,142 +59,148 @@ $othersFormation = new WP_Query($params);
             </div>
             <main id="single_formation_main">
 
-                <div id="single_formation_sidebar_left">
+                <? if (have_posts()) : ?>
+                    <? while (have_posts()) : the_post(); ?>
+                        <div id="single_formation_sidebar_left">
 
-                    <div class="single_formation_thumbnail">
-                        <?php the_post_thumbnail() ?>
-                    </div>
-                    <div id="single_formation_h1">
-                        <h1><?php the_title() ?></h1>
-                    </div>
+                            <div class="single_formation_thumbnail">
+                                <?php the_post_thumbnail() ?>
+                            </div>
+                            <div id="single_formation_h1">
+                                <h1><?php the_title() ?></h1>
+                            </div>
 
-                    <div id="slidebar_element">
-                        <div id="single_formation_tarif">
-                            <img class="single_formation_icone" src="<? echo $icone['euros'] ?>" alt="Pile de pièce de monnaie">
-                            <p><? echo get_post_meta($post->ID, 'tarif_formation', true) ?> euros (€)</p>
+                            <div id="slidebar_element">
+                                <div id="single_formation_tarif">
+                                    <img class="single_formation_icone" src="<? echo $icone['euros'] ?>" alt="Pile de pièce de monnaie">
+                                    <p><? echo the_field('tarif_formation') ?> euros (€)</p>
+                                </div>
+
+                                <div id="single_formation_duree">
+                                    <img class="single_formation_icone" src="<? echo $icone['time'] ?>" alt="">
+                                    <p><? echo the_field('duree_formation') ?> heures</p>
+                                </div>
+
+                                <div id="single_formation_bloc_contact">
+                                    <p>Besoin d’adapter cette formation à vos besoins ?</p>
+                                    <p>N’hésitez pas à nous contacter afin d’obtenir un devis sur mesure ! </p>
+                                    <button onclick="window.location.href ='http://cedille-formation.ftalps.fr/contact/';">Nous contacter</button>
+                                </div>
+                            </div>
                         </div>
 
-                        <div id="single_formation_duree">
-                            <img class="single_formation_icone" src="<? echo $icone['time'] ?>" alt="">
-                            <p><? echo get_post_meta($post->ID, 'duree_formation', true) ?> heures</p>
-                        </div>
+                        <div id="single_formation_content">
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['objectif'] ?>" alt="Cible">
+                                    <h2>LES OBJECTIFS DE LA FORMATION</h2>
+                                </div>
+                                <p><? echo the_field('objectif_formation') ?></p>
+                            </div>
 
-                        <div id="single_formation_bloc_contact">
-                            <p>Besoin d’adapter cette formation à vos besoins ?</p>
-                            <p>N’hésitez pas à nous contacter afin d’obtenir un devis sur mesure ! </p>
-                            <button onclick="window.location.href ='http://cedille-formation.ftalps.fr/contact/';">Nous contacter</button>
-                        </div>
-                    </div>
-                </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['public'] ?>" alt="Foule d'utilisateurs">
+                                    <h2>PROFIL DU PUBLIC CONCERNÉ</h2>
+                                </div>
+                                <h3>TYPE DE PUBLIC</h3>
+                                <p><? echo the_field('type_public') ?></p>
+                                <br />
+                                <h3>PRÉ-REQUIS</h3>
+                                <p><? echo the_field('pre-requis') ?></p>
+                            </div>
 
-                <div id="single_formation_content">
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['objectif'] ?>" alt="Cible">
-                            <h2>LES OBJECTIFS DE LA FORMATION</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'objectif_formation', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['content'] ?>" alt="To Do List">
+                                    <h2>CONTENU DE LA FORMATION</h2>
+                                </div>
+                                <p><? echo the_field('formation_content') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['public'] ?>" alt="Foule d'utilisateurs">
-                            <h2>PROFIL DU PUBLIC CONCERNÉ</h2>
-                        </div>
-                        <h3>TYPE DE PUBLIC</h3>
-                        <p><? echo wpautop(get_post_meta($post->ID, 'type_public', true)) ?></p>
-                        <br />
-                        <h3>PRÉ-REQUIS</h3>
-                        <p><? echo get_post_meta($post->ID, 'pre-requis', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['teachers'] ?>" alt="Icône d'un homme en cravate">
+                                    <h2>LES INTERVENANTS</h2>
+                                </div>
+                                <p><? echo the_field('teachers') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['content'] ?>" alt="To Do List">
-                            <h2>CONTENU DE LA FORMATION</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'formation_content', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['modalities'] ?>" alt="main tenant une roue denté">
+                                    <h2>LES MODALITÉS</h2>
+                                </div>
+                                <h3>Les modalités Pedagogiques</h3>
+                                <p><? echo the_field('modalites_pedagogiques') ?></p> <br>
+                                <h3>Les Modalités Techniques</h3>
+                                <p><? echo the_field('modalites_techniques') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['teachers'] ?>" alt="Icône d'un homme en cravate">
-                            <h2>LES INTERVENANTS</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'teachers', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone" src="<? echo $icone['evaluations'] ?>" alt="point relié par un trait à un drapeau">
+                                    <h2>SUIVI & ÉVALUATIONS</h2>
+                                </div>
+                                <p><? echo the_field('evaluation') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['modalities'] ?>" alt="main tenant une roue denté">
-                            <h2>LES MODALITÉS</h2>
-                        </div>
-                        <h3>Les modalités Pedagogiques</h3>
-                        <p><? echo get_post_meta($post->ID, 'modalites_pedagogiques', true) ?></p> <br>
-                        <h3>Les Modalités Techniques</h3>
-                        <p><? echo get_post_meta($post->ID, 'modalites_techniques', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone2" src="<? echo $icone['access_time'] ?>" alt="Horloge">
+                                    <h2>DÉLAI D'ACCÉS</h2>
+                                </div>
+                                <p><? echo the_field('delai_acces') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone" src="<? echo $icone['evaluations'] ?>" alt="point relié par un trait à un drapeau">
-                            <h2>SUIVI & ÉVALUATIONS</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'evaluation', true) ?></p>
-                    </div>
+                            <div class="single_formation_block">
+                                <div class="single_formation_block_title">
+                                    <img class="single_formation_icone2" src="<? echo $icone['accessibilites'] ?>" alt="Logo accessibilités (personnage bland entoourré d'un riond)">
+                                    <h2>ACCESSIBILITÉS</h2>
+                                </div>
+                                <p><? echo the_field('accessibilites') ?></p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone2" src="<? echo $icone['access_time'] ?>" alt="Horloge">
-                            <h2>DÉLAI D'ACCÉS</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'delai_acces', true) ?></p>
-                    </div>
+                            <div id="satisfaction">
+                                <img class="single_formation_icone" src="<? echo $icone['satisfaction'] ?>" alt="taux de satisfaction">
+                                <p><strong><? echo the_field('taux_satisfaction') ?>% </strong> taux de satisfaction</p>
+                            </div>
 
-                    <div class="single_formation_block">
-                        <div class="single_formation_block_title">
-                            <img class="single_formation_icone2" src="<? echo $icone['accessibilites'] ?>" alt="Logo accessibilités (personnage bland entoourré d'un riond)">
-                            <h2>ACCESSIBILITÉS</h2>
-                        </div>
-                        <p><? echo get_post_meta($post->ID, 'accessibilites', true) ?></p>
-                    </div>
+                            <div class="single_formation_block_title_formation">
+                                <img class="single_formation_icone3" src="<? echo $icone['others'] ?>" alt="icône de 3 classeurs">
+                                <h2>Ces formations peuvent vous intéresser : </h2>
+                            </div>
 
-                    <div id="satisfaction">
-                        <img class="single_formation_icone" src="<? echo $icone['satisfaction'] ?>" alt="taux de satisfaction">
-                        <p><strong><? echo get_post_meta($post->ID, 'taux_satisfaction', true) ?>% </strong> taux de satisfaction</p>
-                    </div>
-
-                    <div class="single_formation_block_title_formation">
-                        <img class="single_formation_icone3" src="<? echo $icone['others'] ?>" alt="icône de 3 classeurs">
-                        <h2>Ces formations peuvent vous intéresser : </h2>
-                    </div>
-
-                    <div id="single_formation_carte_Formation">
-                        <?php $currentID = get_the_ID();
-                        if ($othersFormation->have_posts()) :
-                            while ($othersFormation->have_posts()) : $othersFormation->the_post();
-                                $postID = get_the_ID();
-                                if ($currentID !== $postID) :  ?>
-                                    <div class="single_formation_carte" onclick="window.location.href ='<? the_permalink() ?>'">
-                                            <div class="single_formation_carte_img">
-                                                <?php the_post_thumbnail(); ?>
+                            <div id="single_formation_carte_Formation">
+                                <?php $currentID = get_the_ID();
+                                if ($othersFormation->have_posts()) :
+                                    while ($othersFormation->have_posts()) : $othersFormation->the_post();
+                                        $postID = get_the_ID();
+                                        if ($currentID !== $postID) :  ?>
+                                            <div class="single_formation_carte" onclick="window.location.href ='<? the_permalink() ?>'">
+                                                <div class="single_formation_carte_img">
+                                                    <?php the_post_thumbnail(); ?>
+                                                </div>
+                                                <h2 class="single_formation_carte_title">
+                                                    <?php the_title(); ?>
+                                                </h2>
+                                                <button id="single_formation_link_formation" onclick="window.location.href = '<? the_permalink() ?>'">Découvrir la formation</button>
                                             </div>
-                                            <h2 class="single_formation_carte_title">
-                                                <?php the_title(); ?>
-                                            </h2>
-                                            <button id="single_formation_link_formation" onclick="window.location.href = '<? the_permalink() ?>'">Découvrir la formation</button>
-                                    </div>
-                            <?
-                                endif;
-                            endwhile;
-                            ?>
+                                    <?
+                                        endif;
+                                    endwhile;
+                                    ?>
 
-                        <?php else : ?>
-                            <p>Aucune formation n'a été trouvé dans cette catégorie.</p>
-                        <?php endif; ?>
+                                <?php else : ?>
+                                    <p>Aucune formation n'a été trouvé dans cette catégorie.</p>
+                                <?php endif; ?>
 
-                    </div>
-                </div>
+                            </div>
+                        </div>
+                    <? endwhile; ?>
+                <? else : ?>
+                    <p>Cette formation n'as pas encore de programme ou n'existe pas.</p>
+                <? endif ?>
             </main>
             <script>
                 // DOMElement 
