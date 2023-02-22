a81943b559967d6ce9cf999a428e01fa21414c5c
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-02-22 15:34:01 +0100

    Refacto + rename file

--- assets/js/single-formation.js
+++ assets/js/single-formation.js
@@ -1,57 +1,51 @@
-let sidebar = document.getElementById('single_formation_sidebar_left');
-        let prog = document.getElementById("single_formation_titre");
+let programme = document.getElementById("single_formation_titre");
+let content = document.getElementById("single_formation_content");
+let satisfaction = document.getElementById("satisfaction");
+let element = document.getElementById("slidebar_element");
+let bloc_contact = document.getElementById("single_formation_bloc_contact")
+let others = document.getElementsByClassName("single_formation_block_title_formation")
 
-        let content = document.getElementById("single_formation_content");
-        let contentAfterFix = "single_formation_content_after_fix";
+//Class css
+const block = "single_formation_block";
+const blockTitle = "single_formation_block_title"
 
+//InnerWidth viewport
+let lengthTablettMin = window.innerWidth >= 768;
+let lengthTablettMax = window.innerWidth <= 1100;
 
-        let isMobile = window.innerWidth <= 767;
-        let mobileClass = "sidebar_mobile";
+let isMobile = window.innerWidth <= 767;
+let isTablett = lengthTablettMin & lengthTablettMax;
+let isDesktop = window.innerWidth > 1101;
 
-        let lengthTablettMin = window.innerWidth >= 768;
-        let lengthTablettMax = window.innerWidth <= 1100;
-        let isTablett = lengthTablettMin & lengthTablettMax;
-        let tablettClass = "sidebar_tablett";
+function createSatifactionElementForMobile() {
+    const img = document.querySelector("#satisfaction img")
+    const p = document.querySelector("#satisfaction p")
+    const newDiv = document.createElement("div")
+    const newDiv2 = document.createElement("div")
+    const newH2 = document.createElement("h2")
 
-        let isDesktop = window.innerWidth > 1101;
-        let pcClass = "sidebar_pc";
+    newDiv.classList.add(block)
+    newDiv2.classList.add(blockTitle)
+    newH2.innerText = "TAUX DE SATISFACTION"
 
+    newDiv2.appendChild(img)
+    newDiv2.appendChild(newH2)
+    newDiv.appendChild(newDiv2)
+    newDiv.appendChild(p)
+    satisfaction.appendChild(newDiv)
 
+    //Ajout ou je veux 
+    content.insertBefore(satisfaction, others)
+}
 
-        document.addEventListener('DOMContentLoaded', () => {
-          if (isMobile) {
-            sidebar.classList.add(mobileClass)
-            let mt = sidebar.offsetHeight;
-            prog.style.marginTop = mt + "px";
-          } else if (isTablett) {
-            prog.style.marginTop = 0
-            window.addEventListener('scroll', () => {
-              if (window.scrollY >= 114) {
-                sidebar.classList.add(tablettClass);
-                content.classList.add(contentAfterFix)
-              } else if (window.scrollY <= 113) {
-                if (sidebar.className === tablettClass) {
-                  sidebar.classList.remove(sidebar.className)
-                }
-                if (content.className === contentAfterFix) {
-                  content.classList.remove(content.className)
-                }
-              }
-            })
-          } else if (isDesktop) {
-            prog.style.marginTop = 0
-            window.addEventListener('scroll', () => {
-              if (window.scrollY >= 210) {
-                sidebar.classList.add(pcClass);
-                content.classList.add(contentAfterFix);
-              } else if (window.scrollY <= 209) {
-                if (sidebar.className === pcClass) {
-                  sidebar.classList.remove(sidebar.className)
-                }
-                if (content.className === contentAfterFix) {
-                  content.classList.remove(content.className)
-                }
-              }
-            });
-          }
-        })
+document.addEventListener('DOMContentLoaded', () => {
+    if (isMobile) {
+        createSatifactionElementForMobile()
+    } else if (isTablett) {
+        programme.style.marginTop = 0;
+        element.insertBefore(satisfaction, bloc_contact)
+    } else if (isDesktop) {
+        programme.style.marginTop = 0
+        element.insertBefore(satisfaction, bloc_contact)
+    }
+})
