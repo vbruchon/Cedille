47e3f19dece1391582b5705f10455cf68cd59624
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2023-02-22 15:32:46 +0100

    feat: Add meta description

--- single-formations.php
+++ single-formations.php
@@ -40,6 +40,9 @@ $othersFormation = new WP_Query($params);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ?>">
+    <meta name="description" 
+    content="Bienvenue sur le programme de formation de <?php the_title() ?>. 
+    Vous retrouverez ici toutes les informations utiles comme le prix, la durée, les compétences, etc.">
 </head>
 
 <body>
@@ -202,60 +205,6 @@ $othersFormation = new WP_Query($params);
                     <p>Cette formation n'as pas encore de programme ou n'existe pas.</p>
                 <? endif ?>
             </main>
-            <script>
-                // DOMElement 
-                const programme = document.getElementById("single_formation_titre");
-                const content = document.getElementById("single_formation_content");
-                const satisfaction = document.getElementById("satisfaction");
-                const element = document.getElementById("slidebar_element");
-                const bloc_contact = document.getElementById("single_formation_bloc_contact")
-                const others = document.getElementsByClassName("single_formation_block_title_formation")
-
-                //Class css
-                const block = "single_formation_block";
-                const blockTitle = "single_formation_block_title"
-
-                //InnerWidth viewport
-                const lengthTablettMin = window.innerWidth >= 768;
-                const lengthTablettMax = window.innerWidth <= 1100;
-
-                const isMobile = window.innerWidth <= 767;
-                const isTablett = lengthTablettMin & lengthTablettMax;
-                const isDesktop = window.innerWidth > 1101;
-
-                function createSatifactionElementForMobile() {
-                    const img = document.querySelector("#satisfaction img")
-                    const p = document.querySelector("#satisfaction p")
-                    const newDiv = document.createElement("div")
-                    const newDiv2 = document.createElement("div")
-                    const newH2 = document.createElement("h2")
-
-                    newDiv.classList.add(block)
-                    newDiv2.classList.add(blockTitle)
-                    newH2.innerText = "TAUX DE SATISFACTION"
-
-                    newDiv2.appendChild(img)
-                    newDiv2.appendChild(newH2)
-                    newDiv.appendChild(newDiv2)
-                    newDiv.appendChild(p)
-                    satisfaction.appendChild(newDiv)
-
-                    //Ajout ou je veux 
-                    content.insertBefore(satisfaction, others)
-                }
-
-                document.addEventListener('DOMContentLoaded', () => {
-                    if (isMobile) {
-                        createSatifactionElementForMobile()
-                    } else if (isTablett) {
-                        programme.style.marginTop = 0;
-                        element.insertBefore(satisfaction, bloc_contact)
-                    } else if (isDesktop) {
-                        programme.style.marginTop = 0
-                        element.insertBefore(satisfaction, bloc_contact)
-                    }
-                })
-            </script>
         </body>
         <footer id="footer">
             <? get_footer() ?>
